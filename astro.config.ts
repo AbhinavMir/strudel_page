41fbfe9f6babd1eeaef8e038f4ecdae4711cfbd1
Abhinav Srivastava <atg271@gmail.com>
2024-02-26 23:35:31 -0500

    wip

---
+++ astro.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import UnoCSS from 'unocss/astro';
+
+export default defineConfig({
+  // used to generate images
+  site:
+    'https://abhinavmir.github.io',
+  trailingSlash: 'ignore',
+  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  vite: {
+    optimizeDeps: {
+      exclude: ['@resvg/resvg-js'],
+    },
+  },
+});
